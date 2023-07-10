318f99a0b33790c786f961919849dfa58b335040
RadekZ <radoslawzadroga@wirelesslogic.com>
2023-07-10 15:18:31 +0200

    fixes for Symfony 5.4

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('kronas_smpp_client');
+        $treeBuilder = new TreeBuilder('kronas_smpp_client');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
